be157fda9701bf4590099d898f5c983baab63f20
THEDJA <bd_bouisri@esi.dz>
2015-07-25 15:15:16 +0100

    ProgrammingAssignment2 BOUISRI_Djallel_Ahmed

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,46 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	if(nrow(x) != ncol(x)){
+		stop("x must be a square matrix !")
+	}
+	
+	myInverse <- NULL
+	set <- function(y){
+		if(nrow(x) != ncol(x)){
+			stop("x must be a square matrix !")
+		}
+		x <<- y
+		myInverse <<- NULL
+	}
+	
+	get <- function(){
+		x
+	}
+	
+	setInverse <- function(inverse){
+		if(nrow(x) == nrow(inverse) && ncol(x) == ncol(inverse)){
+			if(all(x * inverse == diag(nrow(x)))){				
+				myInverse <<- inverse
+			}else{				
+				stop("this isn't a correct inverse for x !")
+			}
+		}else{
+			stop("this isn't a correct inverse for x ! (the two matrix aren't compatible in number of rows and columns)")
+		}
+		
+	}
+	
+	getInverse <- function(){
+		myInverse
+	}
+	
+	list(
+		set = set,
+		get = get,
+		setInverse = setInverse,
+		getInverse = getInverse	
+	)
 }
 
 
@@ -12,4 +51,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inverse <- x$getInverse()
+		if(!is.null(inverse)){
+			message("Getting cached inverse of x !")
+			return(inverse)
+		}
+		message("There is no cached result !")
+		inverse <- solve(x$get())
+		x$setInverse(inverse)
+		inverse
 }
