1ff8b0726052c225081e1e28ad9ae051ed6ec2a9
THEDJA <bd_bouisri@esi.dz>
2015-07-25 15:24:29 +0100

    ProgrammingAssignment2 BOUISRI_Djallel_Ahmed

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This object stores a matrix and its cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	## Verifying that x is a true square matrix or stops the execution if not
 	if(nrow(x) != ncol(x)){
 		stop("x must be a square matrix !")
 	}
-	
 	myInverse <- NULL
+	
+	## Changing the matrix implies changing the cached inverse, we must also verify if the new matrix is a square matrix
 	set <- function(y){
 		if(nrow(x) != ncol(x)){
 			stop("x must be a square matrix !")
@@ -21,15 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 		x
 	}
 	
+	## We must verify two points before setting the new value of the cached inverse 
 	setInverse <- function(inverse){
+		## Verify if the two matrix are identical in number of rows and columns
 		if(nrow(x) == nrow(inverse) && ncol(x) == ncol(inverse)){
+			## Verify if X * Inverse_of_X == IDENTITY_MATRIX
 			if(all(x * inverse == diag(nrow(x)))){				
 				myInverse <<- inverse
 			}else{				
 				stop("this isn't a correct inverse for x !")
 			}
 		}else{
-			stop("this isn't a correct inverse for x ! (the two matrix aren't compatible in number of rows and columns)")
+			stop("this isn't a correct inverse for x ! (the two matrix aren't identical in number of rows and columns)")
 		}
 		
 	}
@@ -47,7 +49,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
